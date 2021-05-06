6331b6139cf01e4a457d9c87a18eb54cbb65b556
Adam Mohammed <13.adamm@gmail.com>
2021-05-06 13:04:24 -0400

    added axios and API info

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=cad&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+      .then(res => {
+        setCoins(res.data);
+        console.log(res.data);
+      });
+  });
+
   return (
     <div className="App">
       <h1>API</h1>
